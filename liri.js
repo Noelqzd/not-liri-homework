553720673783e14d719e32ca071132827cb07737
Noel Quezada <noelquezada@gmail.com>
2019-08-30 11:52:22 -0700

    create packages

--- liri.js
+++ liri.js
@@ -1,13 +1,43 @@
-require("").config();
+// require("").config();
 
 //variables
 let request = require("request");
 let fs = require("fs");
-let keys = require ("");
-let Spotify = require('');
-let Spotify = new Spotify();
+// let keys = require ("./keys");
+let moment = require("moment");
+axios = require("axios");
+// let Spotify = require('node-spotify-api');
+// let spotify = new Spotify(keys.spotify);
 
-let userOption = process.argv[2];
-let inputParameter = process.argv[3];
+function findBand(band){
+    var queryString: "https://bandsintownurl" + band + ".com";
+    axios.
+}
 
-userInputs(userOption, inputParameter);
+
+
+function takeQuery(arg1, arg2) {
+    //Switch case
+    switch (arg1) {
+        case  "band":
+        findBand(arg2)
+            break;
+
+        case  "spotify" :
+        findSong(arg2)
+            break;
+        case  "movies": 
+        findMovie(arg2)
+            break;
+
+        case value: ""
+
+            break;
+
+        default:
+            console.log("sorry i can not help you with that")
+            break;
+    }
+}
+
+takeQuery(process.argv[2], process.argv[3]);
