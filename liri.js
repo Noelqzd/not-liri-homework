aa88bbe584a8a3d9ad6abff145e210d32fa54007
Noel Quezada <noelquezada@gmail.com>
2019-09-02 12:51:21 -0700

    added files

--- liri.js
+++ liri.js
@@ -1,43 +1,142 @@
-// require("").config();
+// require("detonev").config();
 
-//variables
-let request = require("request");
+// //letiables
+// let request = require("request");
+
+// const moment = require("moment")
+
+// const fs = require("fs");
+
+// const keys = require("./keys");
+
+// // axios = require("axios");
+
+// const Spotify = require('node-spotify-api');
+// const spotify = new Spotify(keys.spotify);
+
+
+
+// function takeQuery(arg1, arg2) {
+//     //Switch case
+//     switch (arg1) {
+//         case "band":
+//             findBand(arg2)
+//             break;
+
+//         case "spotify":
+//             findSong(arg2)
+//             break;
+//         case "movies":
+//             findMovie(arg2)
+//             break;
+
+//         case value: ""
+
+//             break;
+
+//         default:
+//             console.log("sorry i can not help you with that")
+//             break;
+//     }
+// }
+
+// takeQuery(process.argv[2], process.argv[3]);
+
+require("dotenv").config();
+let keys = require("./keys.js");
+let axios = require("axios");
 let fs = require("fs");
-// let keys = require ("./keys");
-let moment = require("moment");
-axios = require("axios");
-// let Spotify = require('node-spotify-api');
-// let spotify = new Spotify(keys.spotify);
+let Spotify = require('node-spotify-api');
+let spotify = new Spotify(keys.spotify);
+// const moment = require("moment")
 
-function findBand(band){
-    var queryString: "https://bandsintownurl" + band + ".com";
-    axios.
-}
+let searchTerm = process.argv[2];
 
-
-
-function takeQuery(arg1, arg2) {
-    //Switch case
-    switch (arg1) {
-        case  "band":
-        findBand(arg2)
-            break;
-
-        case  "spotify" :
-        findSong(arg2)
-            break;
-        case  "movies": 
-        findMovie(arg2)
-            break;
-
-        case value: ""
-
-            break;
-
-        default:
-            console.log("sorry i can not help you with that")
-            break;
+switch(searchTerm) {
+    case "concert-this":
+        let artist = process.argv.slice(3).join(" ");
+        let concertUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+        axios.get(concertUrl).then(function (response, err) {
+        console.log("Venue Name: " + response.data[0].venue.name);
+        console.log("City: " + response.data[0].venue.city);
+        console.log("Time: " + response.data[0].datetime);
+        
+        
+        //console.log("Time: " + moment(response.data[0].datetime).format("MM/DD/YYYY"));
+        if (err) {
+            console.log("Error: " + err)
+        }
+        })
+      break;
+    case "spotify-this-song":
+        let query = process.argv.slice(3).join(" ");
+        
+        if (query === " ") {
+            console.log("searching for The Sign")
+        }
+        spotify.search({
+            type: "track",
+            query: query
+        }, function (err, data) {
+            if (err) {
+            return console.log("Error:" + err);
+            }
+            console.log("Artist(s): " + data.tracks.items[0].album.artists[0].name);
+            console.log("Song Title: " + data.tracks.items[0].name);
+            console.log("Album Name: " + data.tracks.items[0].album.name);
+            console.log("Listen on Spotify: " + data.tracks.items[0].external_urls.spotify);
+        });
+      break;
+    case "movie-this":
+    //api search
+    let movieQuery = process.argv.slice(3);
+    if (!process.argv[3]) {
+      movieQuery = "Mr. Nobody";
     }
-}
+    let movieUrl = "http://www.omdbapi.com/?t=" + movieQuery + "&apikey=trilogy";
+    axios.get(movieUrl).then(function (response, err) {
+      console.log("Title: " + response.data.Title);
+      console.log("Year Released: " + response.data.Year);
+      console.log("IMDB Rating: " + response.data.imdbRating);
+      console.log("Rotten Tomatoes Rating: " + response.data.Ratings[0].Value);
+      console.log("Movie Was Produced In: " + response.data.Country);
+      console.log("Language(s): " + response.data.Language);
+      console.log("Plot: " + response.data.Plot);
+      console.log("Cast: " + response.data.Actors);
 
-takeQuery(process.argv[2], process.argv[3]);
+      if (err) {
+        console.log("Error: " + err)
+      }
+    })
+      
+      break;
+    case "do-what-it-says":
+    fs.readFile("random.txt", 'utf8', function (error, data) {
+      if (error) {
+        console.log("Error: " + error);
+        return;
+      };
+      let ranString = data.split(',');
+      let ranQuery = ranString[1];
+
+      spotify.search({
+        type: "track",
+        query: ranQuery,
+      }, function (err, data) {
+        if (err) {
+          return console.log("Error:" + err);
+        }
+
+        console.log("Artist: " + data.tracks.items[0].album.artists[0].name);
+        console.log("Song: " + data.tracks.items[0].name);
+        console.log("Appears on: " + data.tracks.items[0].album.name);
+        console.log("Listen on Spotify: " + data.tracks.items[0].preview_url);
+
+      });
+    });
+
+
+      break;
+    default:
+    console.log("Please choose from concert-this, spotify-this-song, movie-this or do-what-it-says");
+  }
